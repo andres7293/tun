09dd28d457eef19fcfc313cdb3731c541061f619
andres <andres7293@gmail.com>
2021-04-21 14:48:42 +0100

    change class name from IP_Header to IPv4_Header

--- IPv4.cc
+++ IPv4.cc
@@ -2,29 +2,29 @@
 #include <bits/stdint-uintn.h>
 #include <cstdint>
 
-uint8_t IP_Header::getVersion(IPv4_Header_t *h) {
+uint8_t IPv4_Header::getVersion(IPv4_Header_t *h) {
     return ((h->version_headerlen & 0xf0) >> 4);
 }
 
-uint8_t IP_Header::getHeaderLen(IPv4_Header_t *h) {
+uint8_t IPv4_Header::getHeaderLen(IPv4_Header_t *h) {
     return (h->version_headerlen & 0x0f);
 }
 
-uint8_t IP_Header::getHeaderLenInBytes(IPv4_Header_t *h) {
+uint8_t IPv4_Header::getHeaderLenInBytes(IPv4_Header_t *h) {
     return getHeaderLen(h) * 4;
 }
 
-uint8_t IP_Header::getFlags(IPv4_Header_t *h) {
+uint8_t IPv4_Header::getFlags(IPv4_Header_t *h) {
     return ((h->flags_fragoffset & 0xE000) >> 13);
 }
 
-uint8_t IP_Header::getFragmentOffset(IPv4_Header_t *h) {
+uint8_t IPv4_Header::getFragmentOffset(IPv4_Header_t *h) {
     return (h->flags_fragoffset & 0x1FFF);
 }
 
-bool IP_Header::validate_checksum(uint8_t *net_frame) {
+bool IPv4_Header::validate_checksum(uint8_t *net_frame) {
     IPv4_Header_t *h = (IPv4_Header_t *) net_frame;
-    uint8_t num_blocks_of_16bits = IP_Header::getHeaderLenInBytes(h) / 2;
+    uint8_t num_blocks_of_16bits = IPv4_Header::getHeaderLenInBytes(h) / 2;
     uint32_t u32_sum = 0;
     uint16_t *p = (uint16_t *) net_frame;
     for (uint8_t i = 0; i < num_blocks_of_16bits; i++)
--- test/IPv4_test.cc
+++ test/IPv4_test.cc
@@ -4,40 +4,40 @@
 /*network ip packet*/
 const uint8_t net_ip_frame_raw[] = {0x45, 0x00, 0x00, 0x54, 0xf2, 0xc8, 0x40, 0x00, 0x40, 0x01, 0x49, 0xde, 0x7f, 0x00, 0x00, 0x01, 0x7f, 0x00, 0x00, 0x01, 0x08, 0x00, 0x4f, 0xff, 0x61, 0xfa, 0x00, 0x01, 0x8f, 0xe1, 0x7f, 0x60, 0x00, 0x00, 0x00, 0x00, 0x76, 0xf0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37};
 
-TEST(IP_Header, getVersion) {
+TEST(IPv4_Header, getVersion) {
     IPv4_Header_t ip_header;
     ip_header.version_headerlen = 0x45;
-    ASSERT_EQ(4, IP_Header::getVersion(&ip_header));
+    ASSERT_EQ(4, IPv4_Header::getVersion(&ip_header));
 }
 
-TEST(IP_Header, getHeaderLen) {
+TEST(IPv4_Header, getHeaderLen) {
     IPv4_Header_t ip_header;
     ip_header.version_headerlen = 0x45;
-    ASSERT_EQ(5, IP_Header::getHeaderLen(&ip_header));
+    ASSERT_EQ(5, IPv4_Header::getHeaderLen(&ip_header));
 }
 
-TEST(IP_Header, getHeaderLenInBytes) {
+TEST(IPv4_Header, getHeaderLenInBytes) {
     IPv4_Header_t ip_header;
     ip_header.version_headerlen = 0x45;
-    ASSERT_EQ(20, IP_Header::getHeaderLenInBytes(&ip_header));
+    ASSERT_EQ(20, IPv4_Header::getHeaderLenInBytes(&ip_header));
 }
 
-TEST(IP_Header, getFlags) {
+TEST(IPv4_Header, getFlags) {
     IPv4_Header_t ip_header;
     ip_header.flags_fragoffset = 0x4000;
-    ASSERT_EQ(0b010, IP_Header::getFlags(&ip_header));
+    ASSERT_EQ(0b010, IPv4_Header::getFlags(&ip_header));
 }
 
-TEST(IP_Header, getFragmentOffset) {
+TEST(IPv4_Header, getFragmentOffset) {
     IPv4_Header_t ip_header;
     ip_header.flags_fragoffset = 0x4000;
-    ASSERT_EQ(0, IP_Header::getFragmentOffset(&ip_header));
+    ASSERT_EQ(0, IPv4_Header::getFragmentOffset(&ip_header));
 }
 
-TEST(IP_Header, validate_checksum) {
-    ASSERT_EQ(true, IP_Header::validate_checksum((uint8_t *)net_ip_frame_raw));
+TEST(IPv4_Header, validate_checksum) {
+    ASSERT_EQ(true, IPv4_Header::validate_checksum((uint8_t *)net_ip_frame_raw));
     uint8_t *zeroBuf = new uint8_t[20];
-    ASSERT_EQ(false, IP_Header::validate_checksum(zeroBuf));
+    ASSERT_EQ(false, IPv4_Header::validate_checksum(zeroBuf));
     delete[] zeroBuf;
 }
 
