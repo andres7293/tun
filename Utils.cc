fca0a691d699804f16bbf5b3f64acaace438d772
andres <andres7293@gmail.com>
2021-04-21 15:15:23 +0100

    mend

---
+++ Utils.cc
@@ -0,0 +1,21 @@
+#include "Utils.H"
+
+uint16_t utils::swap_endian_u16(uint16_t val) {
+    uint16_t result;
+    uint8_t *dest = (uint8_t *) &result;
+    uint8_t *src  = (uint8_t *) &val;
+    dest[0] = src[1];
+    dest[1] = src[0];
+    return result;
+}
+
+uint32_t utils::swap_endian_u32(uint32_t val) {
+    uint32_t result;
+    uint8_t *dest = (uint8_t *) &result;
+    uint8_t *src  = (uint8_t *) &val;
+    dest[0] = src[3];
+    dest[1] = src[2];
+    dest[2] = src[1];
+    dest[3] = src[0];
+    return result;
+}
