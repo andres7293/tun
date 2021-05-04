f4515b15b80fb838e516ffcd733e65042ff178ac
andres <andres7293@gmail.com>
2021-05-04 11:22:32 +0100

    Deletes Allocator. NetBuf creates a buffer in heap

--- src/main.cc
+++ src/main.cc
@@ -10,10 +10,9 @@
 #include "Utils.H"
 #include "TunIf.H"
 #include "NetDev.H"
+#include "NetBuf.H"
 
 using namespace std;
-
-uint8_t buf[1024 * 1];
 
 int main(void) {
     TunIf tunif("tun0");
@@ -23,8 +22,9 @@ int main(void) {
     }
     NetDev netdev(tunif);
     while (1) {
-        ssize_t bytesRead = netdev.read(buf, sizeof(buf));
-        IP::ip_input(netdev, buf, (uint16_t) bytesRead);
+        NetBuf nbuf(1024);
+        ssize_t bytesRead = netdev.read(nbuf.getBuf(), nbuf.getSize());
+        IP::ip_input(netdev, nbuf.getBuf(), (uint16_t) bytesRead);
     }
     tunif.dealloc();
     return 0;
