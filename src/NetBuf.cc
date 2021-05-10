45a99cf09d4df3f1115df28e19a3ce9e1216f999
andres <andres7293@gmail.com>
2021-05-10 12:16:24 +0100

    Creates a class StaticNetBuf which contains the memory block inside object

--- src/NetBuf.cc
+++ src/NetBuf.cc
@@ -28,34 +28,3 @@ void NetBuf::ctor(uint16_t size) {
     this->size = size;
     this->p = new uint8_t[size];
 }
-
-
-StaticNetBuf::StaticNetBuf(MemPool *pool) {
-    this->p = this->pool->alloc();
-    if (this->p == nullptr) {
-        throw StaticNetBufException("Can't alloc more memory in pool");
-    }
-}
-
-StaticNetBuf::StaticNetBuf(StaticNetBuf &n) {
-    this->pool = n.pool;
-    this->p = n.pool->alloc();
-    if (this->p == nullptr) {
-        throw StaticNetBufException("Can't alloc more memory in pool");
-    }
-    memcpy((uint8_t *) this->p, (uint8_t *) n.getBuf(), n.getSize());
-}
-
-StaticNetBuf::~StaticNetBuf() {
-    if (this->p != nullptr) {
-        this->pool->free(this->p);
-    }
-}
-
-uint8_t* StaticNetBuf::getBuf() {
-    return (uint8_t *)this->p;
-}
-
-uint16_t StaticNetBuf::getSize() {
-    return this->pool->getBlockSize();
-}
