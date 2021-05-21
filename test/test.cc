65a3fe18e587e81236ee75fe7f0b7465f299d675
andres <andres7293@gmail.com>
2021-05-21 12:18:19 +0100

    Adds buffer class insipired in boost::buffer. The buffer doesnt owns memory, but provides access to buffer with pointer, size and offsets

--- test/test.cc
+++ test/test.cc
@@ -4,6 +4,7 @@
 #include "RawPointer_test.H"
 #include "Packet_test.H"
 #include "IP_test.H"
+#include "Buffer_test.H"
 
 int main(int argc, char *argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
