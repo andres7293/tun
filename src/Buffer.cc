684af6d9564da3418fa6bb9d60f430262b95efbb
andres <andres7293@gmail.com>
2021-05-21 12:44:58 +0100

    Implements creation of new buffers limiting the size of the original

--- src/Buffer.cc
+++ src/Buffer.cc
@@ -2,13 +2,20 @@
 
 Buffer::Buffer(void* data, unsigned int sizeInBytes, unsigned int offsetInBytes) {
     if (offsetInBytes > sizeInBytes) {
-        this->_data = nullptr;
-        this->_size = 0;
+        this->cancelBuffer();
+        return;
     }
-    else {
-        this->_data = (char*) data + offsetInBytes;
-        this->_size = sizeInBytes - offsetInBytes;
+    this->_data = (char*) data + offsetInBytes;
+    this->_size = sizeInBytes - offsetInBytes;
+}
+
+Buffer::Buffer(Buffer& buffer, int sizeLimit) {
+    if (sizeLimit > buffer.size()) {
+        this->cancelBuffer();
+        return;
     }
+    this->_data = buffer.data();
+    this->_size = sizeLimit;
 }
 
 void* Buffer::data() {
@@ -17,4 +24,9 @@ void* Buffer::data() {
 
 unsigned int Buffer::size() { 
     return this->_size; 
+}
+
+void Buffer::cancelBuffer() {
+    this->_data = nullptr;
+    this->_size = 0;
 }
