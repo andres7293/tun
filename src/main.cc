6f39edd1eff60f88a70b1375dc5b5f4fbb779e08
andres <andres7293@gmail.com>
2021-05-25 14:05:15 +0100

    Implements in main the reading of packets

--- src/main.cc
+++ src/main.cc
@@ -22,9 +22,15 @@ int main(void) {
         cout << "Error opening /dev/tun, errno=" << tunif.getErrno() << endl;
         return -1;
     }
+    NetDev netdev(tunif);
+    IP ip(netdev);
     system("ip link set tun0 up");
     system("ip addr add 10.0.0.1/24 dev tun0");
-    NetDev netdev(tunif);
+    NetBuf nbuf(1024);
+    while (1) {
+        size_t bytesRead = netdev.read(nbuf.data(), nbuf.size());
+        ip.input(nbuf, bytesRead);
+    }
     tunif.dealloc();
     return 0;
 }
