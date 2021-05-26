d8d068b974933b09c22f7574a10a0d38a1fee506
andres <andres7293@gmail.com>
2021-05-26 15:06:05 +0100

    Implements icmp echo_reply and echo_request

--- src/ICMP.cc
+++ src/ICMP.cc
@@ -1,5 +1,7 @@
 #include "ICMP.H"
 #include "Utils.H"
+#include <iostream>
+using namespace std;
 
 ICMP::RetCode ICMP::input(Buffer ip_payload) {
     ICMP::RetCode ret;
@@ -9,7 +11,16 @@ ICMP::RetCode ICMP::input(Buffer ip_payload) {
     ret = this->validateHeaderChecksum(ip_payload);
     if (ret != ICMP::RetCode::OK)
         return ret;
-    return ICMP::RetCode::OK;
+    ICMP_Header *icmph = (ICMP_Header *) ip_payload.data();
+    switch (icmph->type) {
+        case ICMP::ECHO_REQUEST_TYPE:
+            ret = this->echo_request(icmph, ip_payload);
+            break;
+        default:
+                ret = ICMP::RetCode::UNKNOW;
+            break;
+    }
+    return ret;
 }
 
 ICMP::RetCode ICMP::validateHeaderSize(Buffer ip_payload) {
@@ -25,4 +36,18 @@ ICMP::RetCode ICMP::validateHeaderChecksum(Buffer ip_payload) {
         return ICMP::RetCode::OK;
     }
     return ICMP::RetCode::CHECKSUM_ERROR;
+}
+
+ICMP::RetCode ICMP::echo_request(ICMP_Header *icmph, Buffer ip_payload) {
+    if (icmph->code != ICMP::ECHO_REQUEST_CODE) {
+        return ICMP::RetCode::MALFORMED_ICMP_ECHO_REQUEST;
+    }
+    this->echo_reply(icmph, ip_payload);
+    return ICMP::RetCode::ECHO_REPLY;
+}
+
+void ICMP::echo_reply(ICMP_Header *icmp, Buffer ip_payload) {
+    icmp->type = ICMP::ECHO_REPLY_TYPE;
+    icmp->code = ICMP::ECHO_REPLY_CODE;
+    icmp->checksum = utils::checksum(ip_payload.data(), ip_payload.size(), 0);
 }
