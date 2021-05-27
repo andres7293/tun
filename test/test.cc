b7b615bf17c80011e34fef0718cf967a155041b7
andres <andres7293@gmail.com>
2021-05-27 12:31:52 +0100

    Reorganize test order execution

--- test/test.cc
+++ test/test.cc
@@ -1,10 +1,10 @@
 #include <gtest/gtest.h>
 #include "Utils_test.H"
-#include "ICMP_test.H"
-#include "IP_Header_test.H"
-#include "Packet_test.H"
-#include "IP_test.H"
 #include "Buffer_test.H"
+#include "Packet_test.H"
+#include "IP_Header_test.H"
+#include "IP_test.H"
+#include "ICMP_test.H"
 
 int main(int argc, char *argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
