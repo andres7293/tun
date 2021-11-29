3f48a8e00668f092d4143b3635c813e5df8b3875
andres7293 <andres7293@gmail.com>
2021-11-29 21:46:30 +0000

    Empty the packet class

--- src/Packet.cc
+++ src/Packet.cc
@@ -1,17 +1,1 @@
 #include "Packet.H"
-
-int Packet::boundCheck() {
-    uint16_t maxBufSize = this->nbuf.size();
-    uint16_t psize = this->size();
-    if (psize > maxBufSize)
-        return -1;
-    if (this->header.size() > maxBufSize)
-        return -2;
-    if (this->payload.size() > maxBufSize)
-        return -3;
-    return 0;
-}
-
-uint8_t Packet::operator [] (int index) {
-    return this->nbuf.data()[index];
-}
